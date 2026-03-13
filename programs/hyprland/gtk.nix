23c084c2169f3937e7b604c659dcf0726c15a3c6
Atharva-Kapare <atharva.s.kapare@gmail.com>
2026-03-13 21:25:24 +1100

    gtk add

---
+++ programs/hyprland/gtk.nix
@@ -0,0 +1,27 @@
+{pkgs, ...}: {
+  gtk = {
+    enable = true;
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
+
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+      gtk-theme = "adw-gtk3-dark";
+      icon-theme = "Papirus-Dark";
+    };
+  };
+}
