52ee2a0193c47b64acf37a8d640bfd158917754b
Atharva-Kapare <atharva.s.kapare@gmail.com>
2026-04-06 10:41:21 +1000

    Fix: Added MC hosting again

--- device/milano/hosting/default.nix
+++ device/milano/hosting/default.nix
@@ -19,30 +19,30 @@ in {
   virtualisation.podman.dockerCompat = true;
 
   virtualisation.oci-containers.containers = {
-    # minecraft-server = {
-    #   image = "itzg/minecraft-server";
-    #   extraOptions = [
-    #   ];
-    #   ports = [
-    #       "25565:25565"
-    #   ];
-    #   environment = {
-    #     EULA = "TRUE";
-    #     MEMORY = "4G";
-    #   };
-    #   volumes = [
-    #     "/config/minecraft-server:/data"
-    #   ];
-    #   autoStart = true;
-    # };
-
-    playit-gg = {
-      image = "ghcr.io/playit-cloud/playit-agent:0.17";
+    minecraft-server = {
+      image = "itzg/minecraft-server";
       extraOptions = [
-        "--network=host"
+      ];
+      ports = [
+        "25565:25565"
       ];
       environment = {
+        EULA = "TRUE";
+        MEMORY = "4G";
       };
+      volumes = [
+        "/config/minecraft-server:/data"
+      ];
+      autoStart = true;
     };
+
+    # playit-gg = {
+    #   image = "ghcr.io/playit-cloud/playit-agent:0.17";
+    #   extraOptions = [
+    #     "--network=host"
+    #   ];
+    #   environment = {
+    #   };
+    # };
   };
 }
